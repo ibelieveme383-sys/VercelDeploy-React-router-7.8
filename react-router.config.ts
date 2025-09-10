65c3a33b2de321380155863f2e89b5052414f7ff
Johney Sun <ibelieveme383@gmail.com>
2025-09-10 23:11:12 +0900

    I am tried with this.

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,8 +5,8 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  prerender: async () => {
+  prerender: () => {
     return ["/"];
   },
-  presets: [vercelPreset()],
+  presets: [vercelPreset()], 
 } satisfies Config;
