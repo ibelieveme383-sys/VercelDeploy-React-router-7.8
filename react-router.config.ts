a691e4068f96f2e2e1846c735a7fb140b5f1f351
Johney Sun <ibelieveme383@gmail.com>
2025-09-11 00:10:26 +0900

    last once - 2

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ import { vercelPreset } from "@vercel/react-router/vite";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
+  ssr: true,
   prerender: () => {
     return ["/"];
   },
