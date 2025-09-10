11d54881ac7ca8e6d31dc5658fd6348f34a1ad27
Johney Sun <ibelieveme383@gmail.com>
2025-09-11 00:01:09 +0900

    last once

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,10 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
+export const handle = {
+  buildtime: new Date().toISOString(),
+}
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -8,10 +12,10 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: new Date().toISOString() };
-}
+// export function loader({ context }: Route.LoaderArgs) {
+//   return { message: new Date().toISOString() };
+// }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <Welcome message={handle.buildtime} />;
 }
